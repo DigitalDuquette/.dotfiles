fd73bdfa24508fed670584e651e669c39b5f3abf
DigitalDuquette <jared@digitalduquette.com>
2025-07-29 15:58:28 -0400

    Added languages for install script

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -5,7 +5,7 @@ echo "[BOOTSTRAP] Starting full bootstrap sequence..."
 
 BOOTSTRAP_DIR="$HOME/.bootstrap"
 
-for script in 00_homebrew.sh 10_defaults.sh 20_symlinks.sh 90_finish.sh; do
+for script in 00_homebrew.sh 10_defaults.sh 20_symlinks.sh 30_languages.sh 90_finish.sh; do
   SCRIPT_PATH="$BOOTSTRAP_DIR/$script"
   if [ -f "$SCRIPT_PATH" ]; then
     echo "[BOOTSTRAP] Running $script..."
