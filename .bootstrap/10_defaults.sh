d59dc296547e7acad615569bad897c8478fd2e5a
Jared Duquette <jared@padnos.com>
2025-08-03 14:33:40 -0400

    updated defaults to better reflect end state settings

--- .bootstrap/10_defaults.sh
+++ .bootstrap/10_defaults.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 set -euo pipefail
 
+# ====== DEFAULTS =====
 
 echo "[BOOTSTRAP] Setting app defaults with duti..."
 
@@ -12,6 +13,24 @@ duti -s com.colliderli.iina com.microsoft.waveform-audio all
 duti -s com.colliderli.iina public.m3u-playlist all
 
 
+# ====== TRACKPAD =====
+# Enable __three finger drag___ for built-in and Bluetooth trackpads
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+
+# Optional: disable horizontal swipe to avoid gesture conflicts
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+
+# NOTE this may not be safe. However, without this final execution I'm not able to use CLI to apply trackpage changes
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+# leaving it enabled but beware: https://apple.stackexchange.com/questions/456623/macos-command-line-for-track-pad-settings
+
+# Apply changes
+killall cfprefsd
+
+# ===== MENU BAR =====
+
 echo "[BOOTSTRAP] Setting defaults for menu bar spacing..."
 
 defaults -currentHost write -globalDomain NSStatusItemSpacing -int 6
@@ -21,7 +40,35 @@ defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 4
 # defaults -currentHost delete -globalDomain NSStatusItemSpacing
 
 
-echo "[BOOTSTRAP] Setting SYSTEM PREF for Dock..."
+echo "[BOOTSTRAP] Configuring macOS..."
+# The best resource of finding new settings for other users is:
+# https://www.defaults-write.com
+
+osascript -e 'tell application "System Preferences" to quit'
+
+# ====== FINDER ======
+
+# Show Finder path bar:
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show hidden files in Finder (Same as Shift Cmd .)
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Show file extensions in Finder:
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# restart finder to apply all settings
+killall finder || true 
+
+
+# ===== DOCK ======
 
 # only running applications in dock
 defaults write com.apple.dock static-only -bool TRUE
